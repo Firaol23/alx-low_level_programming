88d07edf41565779f6c9c224777d1b292c6e0855
Firaol <dechassafiraol@gmail.com>
2022-12-16 02:22:20 -0800

    10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -14,9 +14,9 @@ _putchar('\n');
 
 else
 {
-for (ro = 0; ro <= (size 1); ro++)
+for (ro = 0; ro <= (size - 1); ro++)
 {
-for (sp = 0; sp < (size 1) - ro; sp++)
+for (sp = 0; sp < (size - 1) - ro; sp++)
 {
 putchar(' ');
 }
