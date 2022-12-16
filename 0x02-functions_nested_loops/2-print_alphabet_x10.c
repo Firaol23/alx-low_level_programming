451f2aa61c0a871392ab0597b6959a54337bc6c7
Firaol <dechassafiraol@gmail.com>
2022-12-16 04:57:02 -0800

    gcc

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints 10 times the alphabet, in lowercase,
+ * followed by a new line
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+	char ch;
+
+	i = 0;
+
+	while (i < 0)
+	{
+		ch = 'a';
+		while (ch <= 'z')
+		{
+			_putchar(ch);
+			ch++;
+	}
+	_putchar('\n');
+	i++;
+}
+}
